575560c9dcd2c17fbea4e1ddf7414cf01944ca6e
mohifk <mohi.fk@hotmail.com>
2022-10-07 14:55:30 +0330

    create table contact in website and complete tables in blog
    tables contact and post

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,17 @@
+
 from django.db import models
 
+
 class post(models.Model):
+    #image=
+    #author=
     title=models.CharField(max_length=255)
     content= models.TextField()
-
+    #tage=
+    #category=
+    count_views= models.IntegerField(default=0)
+    status= models.BooleanField(default=False)
+    publish_date= models.DateTimeField(null=True)
+    create_date= models.DateTimeField(auto_now_add=True)
+    update_date= models.DateTimeField(auto_now=True)
 # Create your models here.
--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
-from django.db import models
 
+from django.db import models
+class Contact(models.Model):
+    name=models.CharField(max_length=255)
+    email=models.EmailField()
+    subject=models.CharField(max_length=255)
+    massage= models.TextField()
+    created_date= models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now=True)
 # Create your models here.
