7a8c5ae27b2e818fd081fc24b503abb9f9a8797e
mohifk <mohi.fk@hotmail.com>
2022-10-07 23:04:33 +0330

    Update admin.py
    edit for change admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from blog.models import post
-admin.site.register(post)
-# Register your models here.
+#@admin.register(post)       # -----this or down
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'create_date'
+    empty_value_display= '-empty-'
+    #fields=('title',)
+    #exclude=('title',)
+    list_display=('title','count_views','status','publish_date')
+    list_filter=('status',)
+    ordering=['-create_date']
+    search_fields=['title','content']
+
+admin.site.register(post,PostAdmin)     # ------ or this or up
+
