6b87583fca02694f090621e55b856e152935ef38
mohifk <mohi.fk@hotmail.com>
2022-10-09 17:45:57 +0330

    edit blog url address  an edit title and  data of single blog

--- blog/views.py
+++ blog/views.py
@@ -12,24 +12,28 @@ def blog_view(request):
    
     context={'posts':posts} ## the means is evry where {%posts%} means post.objects.all()
     return render(request,'blog/blog-home.html',context)
-def blog_single(request):
-    context={'name':'mohi','lastname':'fk'}
-    return render(request,'blog/blog-single.html',context)
-
-def test(request,pid):
-    #Post=post.objects.get(id=pid)
+def blog_single(request,pid):
     Post=get_object_or_404(post,pk=pid)
     Post.count_views=Post.count_views+1
-    #Post.objects.filters(pk=pid).update(count_views=F('count_views')+1)
     Post.save()
-    
-   # p = Paginator(post, 2)
-    #Post.objects.filter(pk=post.pk).update(views=F('views') + 1)
-    
     context={'post':Post}
+   # context={'name':'mohi','lastname':'fk'}
+    return render(request,'blog/blog-single.html',context)
+
+# def test(request,pid):
+#     #Post=post.objects.get(id=pid)
+#     Post=get_object_or_404(post,pk=pid)
+#     Post.count_views=Post.count_views+1
+#     #Post.objects.filters(pk=pid).update(count_views=F('count_views')+1)
+#     Post.save()
+    
+#    # p = Paginator(post, 2)
+#     #Post.objects.filter(pk=post.pk).update(views=F('views') + 1)
+    
+#     context={'post':Post}
   
     
     
-    return render(request,'test.html',context)
+#     return render(request,'test.html',context)
     
 # Create your views here.
