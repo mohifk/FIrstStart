f72bff58389f503f280a6d3c8a6ce7c45df0bb5b
mohifk <mohi.fk@hotmail.com>
2022-10-10 21:38:02 +0330

    edit for ok

--- blog/views.py
+++ blog/views.py
@@ -5,36 +5,18 @@ from django import views
 from django.utils import timezone
 from django.shortcuts import render,get_object_or_404
 from blog.models import post
-# from django.core.paginator import Paginator
+
 def blog_view(request):
     
     #posts=post.objects.filter(status=1)
-    posts=post.objects.filter(publish_date__lte=timezone.now(),status=1)
-   
+    posts=post.objects.filter(publish_date__lte=timezone.now(),status=1) 
     context={'posts':posts} ## the means is evry where {%posts%} means post.objects.all()
     return render(request,'blog/blog-home.html',context)
-def blog_single(request,pid):
-    Post=get_object_or_404(post,pk=pid,status=1)
-    Post.count_views=Post.count_views+1
-    Post.save()
-    context={'post':Post}
-   # context={'name':'mohi','lastname':'fk'}
-    return render(request,'blog/blog-single.html',context)
 
-# def test(request,pid):
-#     #Post=post.objects.get(id=pid)
-#     Post=get_object_or_404(post,pk=pid)
-#     Post.count_views=Post.count_views+1
-#     #Post.objects.filters(pk=pid).update(count_views=F('count_views')+1)
-#     Post.save()
-    
-#    # p = Paginator(post, 2)
-#     #Post.objects.filter(pk=post.pk).update(views=F('views') + 1)
-    
-#     context={'post':Post}
-  
-    
-    
-#     return render(request,'test.html',context)
-    
-# Create your views here.
+
+def blog_single(request,pid):
+    post_=get_object_or_404(post,pk=pid,status=1)
+    post_.count_views+=1
+    post_.save()
+    context={'post':post_}
+    return render(request,'blog/blog-single.html',context)
