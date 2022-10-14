7f849c408f8d85904afe2f03e3b8077bc3d107d1
mohifk <mohi.fk@hotmail.com>
2022-10-14 22:49:52 +0330

    edit for paginator
    +edit views.py for page create
    +edit template blog_home.html  for page create

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django import views
 from django.utils import timezone
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator , EmptyPage , PageNotAnInteger
 
 def blog_view(request,**kwargs):
     posts=Post.objects.filter(publish_date__lte=timezone.now(),status=1) 
@@ -10,6 +11,14 @@ def blog_view(request,**kwargs):
         posts=posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') !=None:
         posts=posts.filter(author__username=kwargs['author_username'])
+    posts= Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts=posts.get_page(page_number)
+    except PageNotAnInteger :
+        posts=posts.get_page(1)
+    except EmptyPage:
+        posts=posts.get_page(1)
     context={'posts':posts}                                  ## the means is evry where {%posts%} means posts
     return render(request,'blog/blog-home.html',context)
 
