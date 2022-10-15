7419a0a1963dabd8eff13d4ba97107b804431c13
mohifk <mohi.fk@hotmail.com>
2022-10-15 21:22:37 +0330

    create form for test

---
+++ website/forms.py
@@ -0,0 +1,9 @@
+import email
+from django import forms
+
+
+class NameForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    email = forms.EmailField()
+    subject = forms.CharField(max_length=255)
+    message = forms.CharField(widget=forms.Textarea)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from website.forms import NameForm
+from website.models import Contact
+
 def Home_page_view(request):
     return render(request,'.\website_temp\index.html')
 def about_view(request):
@@ -8,6 +11,21 @@ def contact_view(request):
     return render(request,'.\website_temp\contact.html')
 def elements_view(request):
     return render(request,'.\website_temp\elements.html')
+
 def test_view(request):
-    return render(request,'.\website_temp\\test.html',{'name':'mohi','lastname':'fk'})
+    if request.method == 'POST' :
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name= form.cleaned_data['name']
+            subject = form.cleaned_data['subject']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            print(name,subject,email,message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse('note valid')
+    
+    form = NameForm()
+
+    return render(request,'test.html',{'form':form})
     
