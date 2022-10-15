7823a518bf55655f58e2f9d7315ba6defdb633b8
mohifk <mohi.fk@hotmail.com>
2022-10-15 13:41:40 +0330

    edit for home_page lastest posts
    +index.html for show post add blog tag

--- blog/templatetags/home_tags.py
+++ blog/templatetags/home_tags.py
@@ -3,6 +3,15 @@ from blog.models import Post, category
 register = template.Library()
 
 @register.inclusion_tag('website_temp/blog_lastpost.html')
-def lastestposts(arg=4):
+def lastestposts(arg=6):
     posts=Post.objects.filter(status=1).order_by('-publish_date')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-category.html')
+def postcategories():
+    posts=Post.objects.filter(status=1)
+    categories = category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories':cat_dict}
