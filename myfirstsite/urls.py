2f58920041eb9350a1afcaec24ce262ce900dbed
mohifk <mohi.fk@hotmail.com>
2022-10-19 07:37:55 +0330

    Update urls.py for robots work
     path('robots.txt', include('robots.urls')

--- myfirstsite/urls.py
+++ myfirstsite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
+
 from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
@@ -32,7 +32,7 @@ urlpatterns = [
     path ('',include('website.urls')),
     path('blog/',include('blog.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
-    path(r'^robots.txt', include('robots.urls')),
+    path('robots.txt', include('robots.urls')),
     ]
     
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
