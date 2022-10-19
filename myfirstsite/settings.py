02dd3db841f6173c0da1b62944142f0e35d12d82
mohifk <mohi.fk@hotmail.com>
2022-10-19 09:56:29 +0330

    add Django debugger toolbar

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.sitemaps',
-
+    'debug_toolbar',
     'django_extensions',
     'website.apps.WebsiteConfig',
     'blog',
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'myfirstsite.urls'
@@ -146,3 +147,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
