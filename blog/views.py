5c1b50eaae78fede1f39d11fda465ec78c88a2b3
mohifk <mohi.fk@hotmail.com>
2022-10-20 22:23:38 +0330

    created form for comment

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post,Comment
 from django.core.paginator import Paginator , EmptyPage , PageNotAnInteger
+from blog.forms import CommentForm
+from django.contrib import messages
 
 def blog_view(request,**kwargs):
     posts=Post.objects.filter(publish_date__lte=timezone.now(),status=1) 
@@ -25,11 +27,18 @@ def blog_view(request,**kwargs):
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
+    if request.method == 'POST' :
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'OK Dude your Comment submit SUCCESS It will be displayed after confirmation')
+        else:
+            messages.add_message(request,messages.ERROR,'Sorry Dude your Comment dident submit')
     posts=Post.objects.filter(status=1) 
     post_=get_object_or_404(Post,pk=pid,status=True)
     post_.count_views+=1
     post_.save()
-    comments=Comment.objects.filter(post=post_.id,approved=1)
+    
     p='Prev Post'
     n='Next Post'
     if pid>1 and pid < len(posts)+1:
@@ -47,8 +56,10 @@ def blog_single(request,pid):
         next1=posts[1]
         prev1=posts[0]
         prev1.title=''
-        p=''                  
-    context={'post':post_,'next1':next1,'prev1':prev1,'p':p,'n':n,'comments':comments}
+        p=''          
+    comments=Comment.objects.filter(post=post_.id,approved=1)  
+    form= CommentForm()      
+    context={'post':post_,'next1':next1,'prev1':prev1,'p':p,'n':n,'comments':comments,'form':form}
     return render(request,'blog/blog-single.html',context)
 
 def test(request):
