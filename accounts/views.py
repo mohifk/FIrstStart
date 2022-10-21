569498af8b8bdd5cc3709b130c5f870ff77b9517
mohifk <mohi.fk@hotmail.com>
2022-10-21 15:20:32 +0330

    authenticated with views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 
 
 def login_view(request):
-    return render(request,'accounts/login.html')
+    if request.user.is_authenticated:
+        msg=f'user is authenticated as {request.user.username}'
+    else:
+        msg='user not authenticated'
+    return render(request,'accounts/login.html',{'msg':msg})
 
 # def logout_view(request):
 #     return
