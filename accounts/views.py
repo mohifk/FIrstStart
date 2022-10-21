cee1517f62667420c5861c6dd51aa2c97c09cde0
mohifk <mohi.fk@hotmail.com>
2022-10-21 15:27:19 +0330

    authenticated by templetes as login.html

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg=f'user is authenticated as {request.user.username}'
-    else:
-        msg='user not authenticated'
-    return render(request,'accounts/login.html',{'msg':msg})
-
+    # if request.user.is_authenticated:
+    #     msg=f'user is authenticated as {request.user.username}'
+    # else:
+    #     msg='user not authenticated'
+    # return render(request,'accounts/login.html',{'msg':msg})
+    return render(request,'accounts/login.html')
+    
 # def logout_view(request):
 #     return
 
